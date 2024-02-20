96f58a4dfbf4091d7bb8b06c32b922d7acf1f860
Alessio Grespi <56415370+AlessioGrespi@users.noreply.github.com>
2024-02-20 13:37:06 +0000

    nav and main hero

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import "../app.css";
+  </script>
+
+  <header class='flex justify-center mx-7 bg-gradient-to-b from-slate-100 to-slate-50 rounded-b-xl '>
+    <nav class='font-semibold flex justify-between max-w-screen-2xl'>
+        <a class='mx-10 text-3xl my-4 hover:bg-slate-200 py-1.5 px-3 rounded-lg' href='./universities'>Universities</a>
+        <a class='mx-10 text-3xl my-4 hover:bg-slate-200 py-1.5 px-3 rounded-lg' href='./courses'>Courses</a>
+        <a class='mx-10 text-3xl my-4 hover:bg-slate-200 py-1.5 px-3 rounded-lg' href='./modules'>Modules</a>
+        <a class='mx-10 text-3xl my-4 hover:bg-slate-200 py-1.5 px-3 rounded-lg' href='./api_access'>API</a>
+    </nav>
+  </header>
+  
+  <slot />
+
+  <footer class='bg-gradient-to-t from-slate-100 to-slate-50 rounded-b-xl '>
+    <div>
+        <h1></h1>
+    </div>
+  </footer>
---
+++ site/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class=' rounded-2xl m-7 flex-col justify-center flex justify-items-center text-center bg-gradient-to-br from-slate-50 to-gray-300 h-[80vh]'>
+    <h1 class='font-semibold text-4xl pb-4'>Find your next uni course</h1>
+    <form action="/action_page.php">
+        <input type="text" class='font-semibold text-xl px-2 py-1 border-slate-100 border-2 rounded-lg w-72' placeholder="Search.." name="search">
+        <button type="submit" class='text-lg -ml-10'>🔎</button>
+      </form>
+</div>
